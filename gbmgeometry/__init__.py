538fbe7a63d1e34fdb7206e22817c8ac8c0cb481
J. Michael Burgess <jmichaelburgess@gmail.com>
2016-07-10 23:37:10 +0200

    added init

---
+++ gbmgeometry/__init__.py
@@ -0,0 +1,22 @@
+from .gbm_detector import NaI0, NaI1, NaI2, NaI3, NaI4, NaI5
+from .gbm_detector import NaI6, NaI7, NaI8, NaI9, NaIA, NaIB
+from .gbm_frame import GBMFrame
+from .gbm import GBM
+from .position_interpolator import PositionInterpolator
+
+__all__=["GBMFrame",
+         "GBM",
+         "NaI0",
+         "NaI1",
+         "NaI2",
+         "NaI3",
+         "NaI4",
+         "NaI5",
+         "NaI6",
+         "NaI7",
+         "NaI8",
+         "NaI9",
+         "NaIA",
+         "NaIB",
+         "PositionInterpolator"
+]
