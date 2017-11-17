b972e4e9244461e7dfe35e4c1512b00f7233dfbd
J. Michael Burgess <jmichaelburgess@gmail.com>
2017-11-17 10:11:56 +0100

    added a GBM det lookup

--- gbmgeometry/__init__.py
+++ gbmgeometry/__init__.py
@@ -7,6 +7,28 @@ from getgbmdata import GetGBMData
 from position_interpolator import PositionInterpolator
 from spacecraft.fermi import Fermi
 
+
+gbm_detector_list = {'n0' : NaI0,
+                     'n1' : NaI1,
+                     'n2' : NaI2,
+                     'n3' : NaI3,
+                     'n4' : NaI4,
+                     'n5' : NaI5,
+                     'n6' : NaI6,
+                     'n7' : NaI7,
+                     'n8' : NaI8,
+                     'n9' : NaI9,
+                     'na' : NaIA,
+                     'nb' : NaIB,
+                     'b0' : BGO0,
+                     'b1' : BGO1,
+
+
+
+}
+
+
+
 from utils.gbm_time import GBMTime
 
 __all__ = ["GBMFrame",
