616838517277840f29ab6d5b2398654bef3c4d3d
J. Michael Burgess <jmichaelburgess@gmail.com>
2020-03-03 15:42:47 +0100

    for got to update files

---
+++ gbmgeometry/test/test_fermi.py
@@ -0,0 +1,23 @@
+from gbmgeometry.spacecraft.fermi import Fermi
+from gbmgeometry import PositionInterpolator
+from gbmgeometry.utils.package_utils import get_path_of_data_file
+
+def test_fermi_constructor():
+
+    trigdat = get_path_of_data_file("glg_trigdat_all_bn080916009_v02.fit")
+
+    interp = PositionInterpolator(trigdat=trigdat)
+
+    fermi = Fermi(interp.quaternion(0), interp.sc_pos(0))
+
+
+
+def test_fermi_plotting():
+
+    trigdat = get_path_of_data_file("glg_trigdat_all_bn080916009_v02.fit")
+
+    interp = PositionInterpolator(trigdat=trigdat)
+
+    fermi = Fermi(interp.quaternion(0), interp.sc_pos(0))
+
+    fermi.plot_fermi()
