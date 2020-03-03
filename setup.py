fa2f4a9a2c68b461f19a3e17296acbdfc632c29c
J. Michael Burgess <jmichaelburgess@gmail.com>
2020-03-03 14:31:16 +0100

    remove non needed things

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-from setuptools.command.build_ext import build_ext as _build_ext
-from Cython.Build import cythonize
-from setuptools import setup, find_packages, Command, Extension
+from setuptools import setup, find_packages, Command
 import os
 import io
 import sys
@@ -8,58 +6,57 @@ from shutil import rmtree
 
 
 # Package meta-data.
-NAME = 'gbmgeometry'
-DESCRIPTION = 'Geometry calculations for Fermi GBM'
-URL = 'https://github.com/mpe-grb/gbmgeometry'
-EMAIL = 'jmichaelburgess@gmail.com'
-AUTHOR = 'J. Michael Burgess'
-REQUIRES_PYTHON = '>=2.7.0'
+NAME = "gbmgeometry"
+DESCRIPTION = "Geometry calculations for Fermi GBM"
+URL = "https://github.com/mpe-grb/gbmgeometry"
+EMAIL = "jmichaelburgess@gmail.com"
+AUTHOR = "J. Michael Burgess"
+REQUIRES_PYTHON = ">=2.7.0"
 VERSION = None
 
 REQUIRED = [
-    'numpy',
-    'matplotlib',
-    'astropy',
-    'sympy',
-    'healpy',
-    'spherical_geometry',
-    'seaborn'
-    
+    "numpy",
+    "matplotlib",
+    "astropy",
+    "sympy",
+    "healpy",
+    "spherical_geometry",
+    "seaborn",
 ]
 
 # What packages are optional?
 EXTRAS = {
-# 'fancy feature': ['django'],
+    # 'fancy feature': ['django'],
 }
 
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
-    
+
 # Load the package's __version__.py module as a dictionary.
 about = {}
 if not VERSION:
-    with open(os.path.join(here, NAME, '__version__.py')) as f:
+    with open(os.path.join(here, NAME, "__version__.py")) as f:
         exec(f.read(), about)
 else:
-    about['__version__'] = VERSION
+    about["__version__"] = VERSION
 
 
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -69,25 +66,22 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds...')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds...")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution...')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution...")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPI via Twine...')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPI via Twine...")
+        os.system("twine upload dist/*")
 
-        self.status('Pushing git tags...')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-        
+        self.status("Pushing git tags...")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
         sys.exit()
-
-
-
 
 
 # Create list of data files
@@ -99,34 +93,29 @@ def find_data_files(directory):
 
         for filename in filenames:
 
-            paths.append(os.path.join('..', path, filename))
+            paths.append(os.path.join("..", path, filename))
 
     return paths
 
 
-#extra_files = find_data_files('/data')
+# extra_files = find_data_files('/data')
 
-        
+
 setup(
-
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=("tests",)),
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-#    package_data={'': extra_files, },    
-    license='BSD',
-    cmdclass={
-        'upload': UploadCommand,},
-    )
-
-
-
+    #    package_data={'': extra_files, },
+    license="BSD",
+    cmdclass={"upload": UploadCommand,},
+)
