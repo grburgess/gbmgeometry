1ab615dd51b3140fb4f01fbd4c392f9d7edae549
grburgess <jmichaelburgess@gmail.com>
2020-03-24 08:12:27 +0100

    update timeout

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,8 @@ todo_include_todos = False
 autosectionlabel_prefix_document = True
 
 # avoid time-out when running the doc
-# nbsphinx_timeout = 30 * 60
+
+nbsphinx_timeout = 30 * 60
 
 # nbsphinx_execute_arguments = [
 #     "--InlineBackend.figure_formats={'svg', 'pdf'}",
