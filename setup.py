18f53975d98753c713cf1fe1b9d25ddeeca4ec87
grburgess <jmichaelburgess@gmail.com>
2020-06-23 15:10:22 +0200

    added data files

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
+import os
 from setuptools import setup
 import versioneer
 
 
+# Create list of data files
+def find_data_files(directory):
+
+    paths = []
+
+    for (path, directories, filenames) in os.walk(directory):
+
+        for filename in filenames:
+
+            paths.append(os.path.join("..", path, filename))
+
+    return paths
+
+
+extra_files = find_data_files("gbmgeometry/data")
 
 
 setup(
     license="BSD",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    scripts = ['bin/get_trigdat', 'bin/get_posthist']
+    include_package_data=True,
+    package_data={"": extra_files},
+    scripts=["bin/get_trigdat", "bin/get_posthist"],
 )
