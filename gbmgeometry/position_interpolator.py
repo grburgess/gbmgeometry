19c2157b8f2d073181773f7d7c818fc31dd8d3f6
grburgess <jmichaelburgess@gmail.com>
2020-07-16 10:52:19 +0200

    added occult checker

--- gbmgeometry/position_interpolator.py
+++ gbmgeometry/position_interpolator.py
@@ -318,6 +318,46 @@ class PositionInterpolator(object):
         names = [f"sc_pos_{s}" for s in ["X", "Y", "Z"]]
         return dict(zip(names, self._scxyz_t(t) * self._factor))
 
+    def is_earth_occulted(self, ra, dec, t):
+
+        earth_radius = 6371.0
+
+        sc_pos = self.sc_pos(t)
+
+        if np.atleast_1d(t).shape[0] == 1:
+
+            fermi_radius = np.sqrt((sc_pos ** 2).sum())
+
+            horizon_angle = 90 - np.rad2deg(np.arccos(earth_radius / fermi_radius))
+
+            min_vis = np.deg2rad(horizon_angle)
+
+            cart_position = ang2cart(ra, dec)
+
+            ang_sep = get_ang(cart_position, -sc_pos)
+
+            return ang_sep < min_vis
+
+        else:
+
+            out = np.ones(len(t), dtype=bool)
+
+            for i, scp in enumerate(sc_pos):
+
+                fermi_radius = np.sqrt((scp ** 2).sum())
+
+                horizon_angle = 90 - np.rad2deg(np.arccos(earth_radius / fermi_radius))
+
+                min_vis = np.deg2rad(horizon_angle)
+
+                cart_position = ang2cart(ra, dec)
+
+                ang_sep = get_ang(cart_position, -scp)
+
+                out[i] = ang_sep < min_vis
+
+            return out
+
     def _interpolate_quaternion(self):
 
         self._quaternion_t = interpolate.interp1d(self._time, self._quats.T)
@@ -416,6 +456,36 @@ class PositionInterpolator(object):
     def normalize(x):
         norm = np.sqrt(np.sum(x ** 2, axis=0))
         return x / norm
+
+
+def ang2cart(ra, dec):
+    """
+    :param ra:
+    :param dec:
+    :return:
+    """
+    pos = np.zeros(3)
+    ra = np.deg2rad(ra)
+    dec = np.deg2rad(dec)
+
+    pos[0] = np.cos(dec) * np.cos(ra)
+    pos[1] = np.cos(dec) * np.sin(ra)
+    pos[2] = np.sin(dec)
+
+    return pos
+
+
+def get_ang(X1, X2):
+    """
+    :param X1:
+    :param X2:
+    :return:
+    """
+    norm1 = np.sqrt(X1.dot(X1))
+    norm2 = np.sqrt(X2.dot(X2))
+    tmp = np.clip(np.dot(X1 / norm1, X2 / norm2), -1, 1)
+
+    return np.arccos(tmp)
 
 
 def slice_disjoint(arr):
